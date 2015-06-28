5b32ba981c828e7990d19e1aa0f1d68f7d852970
Raphael Oliveira <raphael.cps@gmail.com>
2015-06-28 14:58:13 -0300

    Notification

--- SeriesTracker/app/src/main/java/com/movile/next/seriestracker/receiver/ShowUpdateReceiver.java
+++ SeriesTracker/app/src/main/java/com/movile/next/seriestracker/receiver/ShowUpdateReceiver.java
@@ -1,10 +1,16 @@
 package com.movile.next.seriestracker.receiver;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
+import com.movile.next.seriestracker.R;
+import com.movile.next.seriestracker.activity.ShowDetailsActivity;
 import com.movile.next.seriestracker.model.ShowUpdate;
 
 public class ShowUpdateReceiver extends BroadcastReceiver {
@@ -22,6 +28,29 @@ public class ShowUpdateReceiver extends BroadcastReceiver {
 
             //Log.d("Receiver", update.title());
             Toast.makeText(context, update.message(), Toast.LENGTH_SHORT).show();
+
+            Intent pIntent = new Intent(context, ShowDetailsActivity.class);
+            pIntent.putExtra(ShowDetailsActivity.EXTRA_SHOW_SLUG, update.show());
+            pIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            PendingIntent action = PendingIntent.getActivity(
+                    context, 0, pIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(update.title())
+                    .setContentText(update.message())
+                    .setContentIntent(action)
+                    .setAutoCancel(true)
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(update.message()));
+
+            Notification notification = builder.build();
+
+            NotificationManager manager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            manager.notify(0, notification);
+
         }
     }
 }
