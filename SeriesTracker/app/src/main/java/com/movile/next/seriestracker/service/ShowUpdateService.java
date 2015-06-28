1472e42326ff6093682eb40c3b6b2c3479cdc2da
Raphael Oliveira <raphael.cps@gmail.com>
2015-06-28 16:42:56 -0300

    SharedPreferences

--- SeriesTracker/app/src/main/java/com/movile/next/seriestracker/service/ShowUpdateService.java
+++ SeriesTracker/app/src/main/java/com/movile/next/seriestracker/service/ShowUpdateService.java
@@ -2,12 +2,14 @@ package com.movile.next.seriestracker.service;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.content.LocalBroadcastManager;
 
 import com.movile.next.seriestracker.receiver.ShowUpdateReceiver;
 import com.movile.next.seriestracker.model.ShowUpdate;
 import com.movile.next.seriestracker.remote.client.UpdatesClient;
 import com.movile.next.seriestracker.util.ApiConfiguration;
+import com.movile.next.seriestracker.util.Constants;
 
 public class ShowUpdateService extends IntentService {
 
@@ -20,13 +22,21 @@ public class ShowUpdateService extends IntentService {
 
         ShowUpdate update = new UpdatesClient(ApiConfiguration.API_URL_UPDATES).getLatest();
 
+        SharedPreferences preferences = this.getSharedPreferences(Constants.COMMON_SHARED_PREFS, MODE_PRIVATE);
+        String lastUpdate = preferences.getString(Constants.KEY_LAST_UPDATE, null);
+        if (lastUpdate == null || !lastUpdate.equals(update.date())) {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(Constants.KEY_LAST_UPDATE, update.date());
+            editor.commit();
+
+            Intent bIntent = new Intent(ApiConfiguration.BROADCAT_ACTION_SHOW_UPDATE);
+            bIntent.putExtra(ShowUpdateReceiver.EXTRA_UPDATE, update);
+            //LocalBroadcastManager.getInstance(this).sendBroadcast(bIntent);
+            sendBroadcast(bIntent);
+        }
+
         //Log.d("Service", update.title());
         //Toast.makeText(this, "Oi", Toast.LENGTH_SHORT).show();
-
-        Intent bIntent = new Intent(ApiConfiguration.BROADCAT_ACTION_SHOW_UPDATE);
-        bIntent.putExtra(ShowUpdateReceiver.EXTRA_UPDATE, update);
-        //LocalBroadcastManager.getInstance(this).sendBroadcast(bIntent);
-        sendBroadcast(bIntent);
 
     }
 }
---
+++ SeriesTracker/app/src/main/java/com/movile/next/seriestracker/util/Constants.java
@@ -0,0 +1,11 @@
+package com.movile.next.seriestracker.util;
+
+/**
+ * Created by movile on 28/06/15.
+ */
+public abstract class Constants {
+
+    public static final String COMMON_SHARED_PREFS = "common";
+
+    public static final String KEY_LAST_UPDATE = "last_update";
+}
